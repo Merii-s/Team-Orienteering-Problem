e56d4f50ee561a1b7f81e54af3bb8aabbefbf2e7
ibenyago <imene.benyagoub@etu.utc.fr>
2023-12-05 12:35:37 +0100

    change type attributs class TestInstance

--- src/main.py
+++ src/main.py
@@ -2,26 +2,43 @@ import matplotlib.pyplot as plt
 import test_instances as ti
 from pathlib import Path  # Pour manipuler les chemins de fichiers
 from glob import glob  # Pour rechercher des fichiers
+import random
 
 
 def main():
-    Chao_directory = "TestInstances\Chao"  
-    Tsiligirides_directory = "TestInstances\Tsiligirides"  
+    Chao_directory = "TestInstances\Chao"
+    Tsiligirides_directory = "TestInstances\Tsiligirides"
 
     Tsiligirides = ti.read_test_sets(Tsiligirides_directory)
     Chao = ti.read_test_sets(Chao_directory)
 
-    # Print the contents of the dictionary for verification
-    print("Tsiligirides :\n")
-    for test_set, instances in Tsiligirides.items():
-        print(f"Test Set: {test_set}")
-        for instance in instances:
-            print(instance)
-    print("Chao :\n")
-    for test_set, instances in Chao.items():
-        print(f"Test Set: {test_set}")
-        for instance in instances:
-            print(instance)
+    # # Print the contents of the dictionary for verification
+    # print("Tsiligirides :\n")
+    # for test_set, instances in Tsiligirides.items():
+    #     print(f"Test Set: {test_set}")
+    #     for instance in instances:
+    #         print(instance)
+    # print("Chao :\n")
+    # for test_set, instances in Chao.items():
+    #     print(f"Test Set: {test_set}")
+    #     for instance in instances:
+    #         print(instance)
+
+    print(
+        sum([len(Tsiligirides[i]) for i in Tsiligirides] + [len(Chao[i]) for i in Chao])
+    )
+
+    # Récupérer une clé aléatoire pour Tsiligirides
+    random_key_t = random.choice(list(Tsiligirides.keys()))
+
+    # Récupérer une clé aléatoire pour Chao
+    random_key_c = random.choice(list(Chao.keys()))
+
+    # Utiliser la clé aléatoire pour tracer les points d'une instance
+    ti.plot_instance_points(random.choice(Tsiligirides[random_key_t]))
+    ti.plot_instance_points(random.choice(Chao[random_key_c]))
+
+
 
 
 if __name__ == "__main__":
--- src/test_instances.py
+++ src/test_instances.py
@@ -1,31 +1,31 @@
-from typing import List, Tuple
-from dataclasses import dataclass
-from glob import glob
 import os
+from glob import glob
+from dataclasses import dataclass
+from typing import List
 import matplotlib.pyplot as plt
 
 @dataclass
 class TestInstance:
     name: str
-    n: int  # number of vertices
+    n: int  # number of clients + 2 (depot and end)
     m: int  # number of vehicules
     tmax: float  # available time budget per path
-    data: List[Tuple[float, float, int]]  # list of (x, y, score) tuples
-
+    x_coordinates: List[float]  # x coordinates
+    y_coordinates: List[float]  # y coordinates
+    scores: List[int]  # scores
+    
     def __repr__(self) -> str:
-        return f"TestInstance(name='{self.name}', n={self.n}, m={self.m}, tmax={self.tmax}, data={self.data})"
+        return f"TestInstance(name='{self.name}', n={self.n}, m={self.m}, tmax={self.tmax}, " \
+               f"x_coordinates={self.x_coordinates}, y_coordinates={self.y_coordinates}, scores={self.scores})"
 
     def plot_instance(self):
-        x_values = [x for x, _, _ in self.data]
-        y_values = [y for _, y, _ in self.data]
         plt.figure(figsize=(8, 6))
-        plt.scatter(x_values, y_values, s=50)
+        plt.scatter(self.x_coordinates, self.y_coordinates, s=50)
         plt.title(f"Instance: {self.name}")
         plt.xlabel("X Coordinate")
         plt.ylabel("Y Coordinate")
         plt.grid(True)
         plt.show()
-
 
 def read_test_sets(dirpath: str):
     test_sets = glob(os.path.join(dirpath, "Set*"))
@@ -38,11 +38,27 @@ def read_test_sets(dirpath: str):
             m = int(f.readline().split(" ")[-1])
             tmax = float(f.readline().split(" ")[-1])
             data = [tuple(map(float, line.strip().split())) for line in f.readlines()]
+            
+            # Separate data into x, y, and score lists
+            x_coordinates = [x for x, _, _ in data]
+            y_coordinates = [y for _, y, _ in data]
+            scores = [score for _, _, score in data]
+            
         if test_set not in test_dict:
             test_dict[test_set] = []
-        instance = TestInstance(name=name, n=n, m=m, tmax=tmax, data=data)
+        instance = TestInstance(name=name, n=n, m=m, tmax=tmax,
+                                x_coordinates=x_coordinates, y_coordinates=y_coordinates, scores=scores)
         test_dict[test_set].append(instance)
         # Plot each instance after creation
         # instance.plot_instance()
     return test_dict
 
+
+def plot_instance_points(instance: TestInstance):
+    plt.figure(figsize=(8, 6))
+    plt.scatter(instance.x_coordinates, instance.y_coordinates, s=50)
+    plt.title(f"Instance: {instance.name}")
+    plt.xlabel("X Coordinate")
+    plt.ylabel("Y Coordinate")
+    plt.grid(True)
+    plt.show()
